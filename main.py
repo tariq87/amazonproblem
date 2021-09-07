90371a40e2ec340588731ebedbfb173e88dab35e
tariq sidd <tariq.siddiqui@r1soft.com>
2021-09-07 05:26:50 +0000

    Left shift array by n spaces

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-#Fibonacci Series
-def fibnac(s):
-  n = len(s)
-  fib = [0]*(n+1)
-  fib[1] = 1
-  sm = fib[0]+fib[1]
-  for i in range(2,n+1):
-    fib[i] = fib[i-1]+fib[i-2]
-    sm += fib[i]
-  fin = list(zip(s,map(str,fib[1:])))
-  return str(sm)+"".join(sum(fin,()))
+#left shift array by N places
+def leftshift(arr,n):
+  n = n%len(arr)
+  print(arr[n:]+arr[:n])
 
-
-print(fibnac("beautiful"))
+leftshift([1,2,3,4,5],111)
